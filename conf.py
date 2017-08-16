7f1448c23deeaebb7767d5e50eaf43d8c2ecdae6
Nabil Freij <nabil.freij@gmail.com>
2017-08-16 17:17:14 +0200

    sunpy title

--- conf.py
+++ conf.py
@@ -45,7 +45,7 @@ from sunpy_sphinx_theme.conf import *
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_title = 'SunPy Website'
+html_title = 'SunPy'
 
 html_static_path = ['_static']
 
